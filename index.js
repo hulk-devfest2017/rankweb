d174d2573275fb7506aba97f673509b4ef3a73a2
devsheder <adrien.lasselle@gmail.com>
2017-06-29 16:10:08 +0200

    implement getting results from MongoDB instance

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+// Requirements
 var Express = require('express');
 var app = Express();
+var Q = require('q');
+
+// Configuration
 var mockedMode = false;
 var serverPort = 3000;
-var Q = require('q');
+
+// Constantes
+var constantes = {
+  ERRORS : {
+    NOT_FOUND : "NOT_FOUND"
+  }
+}
 
 // Node script params
 process.argv.forEach(function (val, index) {
@@ -13,7 +23,7 @@ process.argv.forEach(function (val, index) {
 
 // Injections
 var HulkChallengeService = require("./services/HulkChallenge.service")
-var hulkChallengeService = new HulkChallengeService(Q, mockedMode);
+var hulkChallengeServiceInstance = new HulkChallengeService(Q, mockedMode, constantes);
 
 // Web application ressources
 app.get('/', function (req, res) {
@@ -24,15 +34,19 @@ app.get('/', function (req, res) {
 // Rest ressources
 app.get('/v1/hulk-challenge/results', function (req, res) {
   // Get all of the results of the Hulk Challenge
-  hulkChallengeService.getAllResults().then(function(results) {
+  hulkChallengeServiceInstance.getAllResults().then(function(results) {
     res.status(200).json(results);
   }).fail(function(error) {
-    res.status(404).send("No results found");
+    if (constantes.ERRORS.NOT_FOUND === error) {
+      res.status(404).send("No results found");
+    } else {
+      res.status(500).send(error);
+    }
   });
 });
 
 
-// Staring server
+// Starting server
 app.listen(serverPort);
 
 var startMessage = "Server started on port " + serverPort;
--- services/HulkChallenge.service.js
+++ services/HulkChallenge.service.js
@@ -1,17 +1,31 @@
 /**
  * Service to access Hulk Challenge datas
- * @param Q{Object} promise library
- * @param mockedMode{boolean} indicates if the service is in mocked mode or not
+ * @param $q{Object} promise library
+ * @param $mockedMode{boolean} indicates if the service is in mocked mode or not
+ * @param $constantes{Json} constantes of the project
  */
-var HulkChallengeService = function (Q, mockedMode) {
-    var MongoClient, assert, allResults;
+var HulkChallengeService = function ($q, $mockedMode, $constantes) {
+    var MongoClient, allResults;
 
-    if (mockedMode) {
+    if ($mockedMode) {
         allResults = require("./mock/results");
     } else {
         MongoClient = require('mongodb').MongoClient;
-        assert = require('assert');
-        // TODO Mongo connection
+    }
+
+    /**
+     * Error manager
+     * @param deferred{Promise} The promise to reject
+     * @param err{Object} Error object from MongoDB API
+     * @param message{String} message of the error
+     */
+    var _manageError = function(deferred, err, message) {
+        var newMessage = message;
+        if (err) {
+            newMessage += " > " + JSON.stringify(err);
+        }
+        console.error(newMessage);
+        deferred.reject(message);
     }
 
     /**
@@ -19,12 +33,37 @@ var HulkChallengeService = function (Q, mockedMode) {
      * @return {Promise}
      */
     this.getAllResults = function() {
-        var deferred = Q.defer();
-        if (mockedMode) {
+        var deferred = $q.defer();
+        if ($mockedMode) {
             deferred.resolve(allResults);
         } else {
-            // TODO Mongo request
-            deferred.reject("Not found");
+            // Default port is in /etc/mongod.conf
+            var url = 'mongodb://localhost:27017/hulkdevfestgame';
+
+            // Try connect to the Server 
+            MongoClient.connect(url, function(errConnection, db) {
+                if (errConnection) {
+                    // Error
+                    return _manageError(deferred, errConnection, "Error while triyng connect to the DB");
+                }
+                
+                // Get all results
+                db.collection('results').find({}).toArray(function(errFind, results) {
+                    if (errFind) {
+                        // Error
+                        db.close();
+                        return _manageError(deferred, errFind, "Error while getting all the results");
+                    } else if (results.length === 0) {
+                        // Not result found
+                        db.close();
+                        return _manageError(deferred, null, $constantes.ERRORS.NOT_FOUND);
+                    } else {
+                        // At least one result found
+                        deferred.resolve(results);
+                        db.close();
+                    }
+                });
+            });
         }
         return deferred.promise;
     }
